4cae0ad4fea51c8af4b1165f4de2e4554f6a2ff1
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-20 16:11:38 +0300

    create a single main entry point (main.ts) that handles both a traditional HTTP server (running on a specific port) and a RabbitMQ microservice

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,22 +1,22 @@
 
 import { NestFactory } from '@nestjs/core';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
 
-  // const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix("api");
-  // app.enableCors({
-  //   origin:'http://localhost:3000'
-  // })
-  // await app.listen(process.env.PORT ?? 8001);
+  const app = await NestFactory.create(AppModule);
 
-  
+  app.setGlobalPrefix("api");
+  app.enableCors({
+    origin: 'http://localhost:3000'
+  })
+  await app.listen(process.env.PORT ?? 8001);
+
   //---------------------------------
   // this micro service is listening 
   //---------------------------------
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.RMQ,
     options: {
       urls: ['amqps://yljkafvf:XEOMZLr5GTJAxWb_-OVAeUn0IG3qN1Lf@hummingbird.rmq.cloudamqp.com/yljkafvf'],
@@ -27,7 +27,7 @@ async function bootstrap() {
     },
   });
 
-  app.listen();
+  microservice.listen();
 }
 bootstrap();
 
