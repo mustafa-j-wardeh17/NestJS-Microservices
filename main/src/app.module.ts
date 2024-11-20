3342d5903e99969587b6531262e0e118689b0207
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-20 11:55:31 +0300

    set mongoose product schema for main microservice

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://admin:adminpassword@localhost:27017', {
     dbName: 'nest_main',
     autoCreate: true
-  })],
+  }), ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ main/src/product/product.model.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Product {
+    @Prop()
+    id: number;
+
+    @Prop()
+    title: string;
+
+    @Prop()
+    image: string;
+
+    @Prop()
+    likes: number
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product)
+// automatically generates the corresponding Mongoose schema from the Product class decorated with @Prop() and @Schema() decorators.
