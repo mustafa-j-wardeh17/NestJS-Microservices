3516ae0524fcbacd474f746cd656959dfedfd77d
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-22 11:18:15 +0300

    Prevent hydration from theme provider

--- frontend/components/theme-provider.tsx
+++ frontend/components/theme-provider.tsx
@@ -1,5 +1,7 @@
 'use client'
-
+//---------------------------------------
+//-------Prevent Hydration Errors--------
+//---------------------------------------
 import * as React from 'react'
 const NextThemesProvider = dynamic(
     () => import('next-themes').then((e) => e.ThemeProvider),
@@ -14,3 +16,19 @@ import dynamic from 'next/dynamic'
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
+
+//--------------------------------------
+//----------Hydration Error-------------
+//--------------------------------------
+// "use client"
+
+// import * as React from "react"
+// import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+// export function ThemeProvider({
+//   children,
+//   ...props
+// }: React.ComponentProps<typeof NextThemesProvider>) {
+//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+// }
